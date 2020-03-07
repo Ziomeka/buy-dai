36efd40edd09b3d6c50a312b02f79a40cc0b3e9f
Agnieszka Ziomek <poziomkaa@gmail.com>
2020-03-06 22:59:41 -0800

    refactor: Navigation component cleanup

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,36 +14,19 @@
       v-model="drawer"
       app
     >
-    <v-list dense>
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/">Home</router-link>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/AllOffers">Browse offers</router-link>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/MyOffers">My offers</router-link>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/settings">Settings</router-link>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+    <v-list>
+      <v-list-item
+        v-for="link in links"
+        :key="link.text"
+        router :to="link.route"
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            {{link.text}}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -53,6 +36,12 @@ export default {
   name: 'Navigation',
   data: () => ({
     drawer: null,
+    links: [
+      { text: 'Home', route: '/' },
+      { text: 'Browse offers', route: '/AllOffers' },
+      { text: 'My offers', route: '/MyOffers' },
+      { text: 'Settings', route: '/Settings' },
+    ],
   }),
 };
 </script>
