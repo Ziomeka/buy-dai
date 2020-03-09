9c51a63efabe8cea8e3b3f68179b328b6fa8480c
adam <adam.skrodzki@paragmaticcoders.com>
2020-03-09 14:05:21 -0700

    changes

--- src/main.js
+++ src/main.js
@@ -18,13 +18,13 @@ Vue.prototype.$blockchain = {
   submitter: undefined,
 };
 
-portis.isLoggedIn().then(({ err, result }) => {
-  if (result == false) {
+portis.isLoggedIn().then(({ result }) => {
+  if (result === false) {
     portis.showPortis().then(() => {
-      portis.changeNetwork('kovan')
-    })
+      portis.changeNetwork('kovan');
+    });
   }
-})
+});
 
 new Vue({
   router,
