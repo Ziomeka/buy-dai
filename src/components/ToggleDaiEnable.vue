4d8588aa3c2f5948182eb95aaff50b0373b5d527
Agnieszka Ziomek <poziomkaa@gmail.com>
2020-03-10 19:14:09 -0700

    feature: Add proper labels and behaviour to switch

--- src/components/ToggleDaiEnable.vue
+++ src/components/ToggleDaiEnable.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <v-switch label="Enable DAI" @change="changeState"></v-switch>
+    <v-switch
+      :label="switchLabel"
+      :loading="isEnablingPending"
+      :disabled="isEnablingPending"
+      :value="switchPosition"
+      @change="changeState" >
+    </v-switch>
     {{isDaiEnabled}}
   </div>
 </template>
@@ -11,7 +17,21 @@ import { mapState, mapActions } from 'vuex';
 export default {
   name: 'ToggleDaiEnable',
   computed: {
-    ...mapState('blockchainUser', ['isDaiEnabled']),
+    ...mapState('blockchainUser', ['isDaiEnabled', 'isEnablingPending']),
+    switchPosition() {
+      return this.isEnablingPending ? !this.isDaiEnabled : this.isDaiEnabled;
+    },
+    switchLabel() {
+      let label;
+      if (this.isEnablingPending) {
+        label = 'Loading';
+      } else if (this.isDaiEnabled) {
+        label = 'Disable DAI';
+      } else {
+        label = 'Enable DAI';
+      }
+      return label;
+    },
   },
   methods: {
     ...mapActions('blockchainUser', ['getState']),
