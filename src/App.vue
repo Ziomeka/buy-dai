287cf43a43c7661d35cfc81376fc8eb02b47e713
Agnieszka Ziomek <poziomkaa@gmail.com>
2020-03-19 15:52:28 +0100

    feature: Add different primary color for different user types

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,35 @@
 
 <script>
 import Navigation from '@/components/Navigation.vue';
+import { mapState } from 'vuex';
 
 export default {
   name: 'App',
-
   components: {
     Navigation,
   },
-
   data: () => ({
     drawer: null,
   }),
+  computed: {
+    ...mapState('blockchainUser', ['userType']),
+  },
+  methods: {
+    setPrimaryColor() {
+      if (this.userType === 'traveler') {
+        this.$vuetify.theme.themes.light.primary = '#2c7fce';
+      } else {
+        this.$vuetify.theme.themes.light.primary = '#f6a81e';
+      }
+    },
+  },
+  mounted() {
+    this.setPrimaryColor();
+  },
+  watch: {
+    userType() {
+      this.setPrimaryColor();
+    },
+  },
 };
 </script>
