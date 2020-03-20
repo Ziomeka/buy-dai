00267ad21c345f46be81335ac656193e0fd324b3
Agnieszka Ziomek <poziomkaa@gmail.com>
2020-03-20 09:23:07 +0100

    feature: add airport dropdown with static data

--- src/components/NewOffer.vue
+++ src/components/NewOffer.vue
@@ -4,6 +4,16 @@
     <v-card max-width="600" class="mx-auto">
       <v-card-title class="headline">Creating offer</v-card-title>
       <v-form class="pa-5" v-model="valid">
+        <v-autocomplete
+          label="Choose airport"
+          no-data-text="Aiport not found"
+          :items="items"
+          item-text="name"
+          item-value="code"
+          return-object
+          v-model="airport"
+        ></v-autocomplete>
+        {{airport.name}}
         <v-text-field
           v-model="dai"
           label="Amount of DAI you want to change"
@@ -41,6 +51,21 @@ export default {
     valid: false,
     dai: null,
     dolars: null,
+    airport: {},
+    items: [
+      {
+        id: 1,
+        name: 'dupa',
+        code: 'XYZ',
+        currency: 'dolar',
+      },
+      {
+        id: 2,
+        name: 'kupka',
+        code: 'ABC',
+        currency: 'euro',
+      },
+    ],
   }),
 };
 </script>
