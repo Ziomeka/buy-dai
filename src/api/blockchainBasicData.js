b1a8ceb5e8b5e5dfd446c1c786464bc4beca867a
adam <adam.skrodzki@paragmaticcoders.com>
2020-03-26 16:34:51 +0100

    fix web3@1.X new block

--- src/api/blockchainBasicData.js
+++ src/api/blockchainBasicData.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 function factory(rec, store) {
+  let is1X = false;
+  let subscription = undefined;
   let latestBlockNumber = 0;
   let latestBlockHash = 0;
   // const marketAddress = '0xBB676E00BF32cADf7bB08cd77eE519f19194b07E';
@@ -16,7 +18,8 @@ function factory(rec, store) {
       
       });
     } else {
-      web3.eth.net.getNetworkType().then((err,res) => {
+      is1X = true;
+      web3.eth.net.getNetworkType().then((res,err) => {
         
         if (store.state.networkStats.networkName !== res) {
           store.commit('networkStats/setNetwork', res);
@@ -25,27 +28,43 @@ function factory(rec, store) {
       });
     }
   }
+  function validateNewBlock(inputHash, latestBlockNumber) {
+    if (inputHash !== latestBlockHash) {
+      store.dispatch('networkStats/setBlockNumber', latestBlockNumber);
+      latestBlockHash = inputHash;
+    }
+  }
   function getLatestBlock() {
     const web3 = rec.getWeb3();
     if (!web3) return;
-    web3.eth.getBlockNumber((err, res) => {
-      if (err) {
-        console.log(err);
-      }
-      if (latestBlockNumber !== res) {
-        web3.eth.getBlock(res, (e, r) => {
-          if (!e && r && r.hash !== latestBlockHash) {
-            store.dispatch('networkStats/setBlockNumber', latestBlockNumber);
-            latestBlockHash = r.hash;
+    if(is1X && !subscription){
+      subscription = web3.eth.subscribe('newBlockHeaders' ,(err, result)=>{
+        validateNewBlock(result.hash, result.number);
+      })
+      .on("data", ()=>{
+      })
+      .on("error", console.error);
+    }else{
+      if(!is1X){
+        web3.eth.getBlockNumber((err, res) => {
+          if (err) {
+            console.log(err);
           }
+          if (latestBlockNumber !== res) {
+            web3.eth.getBlock(res, (e, r) => {
+              if (!e && r && r.hash !== latestBlockHash) {
+                store.dispatch('networkStats/setBlockNumber', latestBlockNumber);
+                latestBlockHash = r.hash;
+              }
+            });
+          }
+          latestBlockNumber = res;
         });
       }
-      latestBlockNumber = res;
-    });
+    }
   }
-
-  getLatestBlock();
   fetchNetwork();
+  setInterval(getLatestBlock, 3000);
 
   return {
     fetchNetwork,
