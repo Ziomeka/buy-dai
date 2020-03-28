8d90684e1dca29009dc093f810f8dfdfcc325656
pragmaticadam <adam.skrodzki@pragmaticcoders.com>
2020-03-28 13:41:54 +0000

    extend stre

--- src/store/modules/networkStats.js
+++ src/store/modules/networkStats.js
@@ -5,18 +5,59 @@ const s = {
 };
 
 const mutations = {
-  setBlockDetails(payload) {
-    s.blockNumber = payload;
+  setAccount(state, payload) {
+    state.account = payload;
+  },
+  setNetwork(state, payload) {
+    if (Number.isNaN(payload)) {
+      state.networkName = payload;
+    } else {
+      const networkId = parseInt(payload.toString(), 10);
+      if (networkId === 1) {
+        state.networkName = 'main';
+      } else
+      if (networkId === 42) {
+        state.networkName = 'kovan';
+      } else {
+        state.networkName = 'invalid';
+      }
+    }
+  },
+  setBlockDetails(state, payload) {
+    state.blockNumber = payload;
   },
 };
 
 const actions = {
-  setBlockNumber({ commit }) {
-    commit('setBlockNumber', true);
+  setBlockNumber({ dispatch, commit }, payload) {
+    if (payload !== s.blockNumber) {
+      // read new events
+      dispatch('readEvents', {
+        old: s.blockNumber,
+        new: payload,
+      });
+    }
+    commit('setBlockDetails', payload);
   },
-  updateAccountInfo({ commit, state},w3){
-
-  }
+  setNetwork({ commit }, payload) {
+    commit('setNetwork', payload);
+  },
+  readEvents(context, payload) {
+    let { old } = payload;
+    if (payload.old === 0) {
+      old = payload.new - 100000;
+    }
+    if (payload.new - payload.old < 10) {
+      old = payload.new - 10;
+    }
+    console.log('readEvents', old, payload.new);
+  },
+  updateAccountInfo({ commit }, w3) {
+    w3.eth.getAccounts((e, acc) => {
+      console.log('Acc found', acc[0]);
+      commit('setAccount', acc[0]);
+    });
+  },
 };
 
 export default {
