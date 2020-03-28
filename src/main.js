77b9829c84296ecae1910755e3728b28a48a27c8
adam <adam.skrodzki@paragmaticcoders.com>
2020-03-28 15:50:04 +0100

    Portis injected priority fix

--- src/main.js
+++ src/main.js
@@ -12,17 +12,15 @@ import blockchainBasicDataFactory from './api/blockchainBasicData';
 
 let initialized = false;
 
-function initOrUpdateVue(portisWeb3, portisLogged, portisInstance) {
+function setBlckchainInPrototype(w3, isLogged, instance) {
   let rec;
-  Vue.config.productionTip = false;
-
   if (Vue.prototype.$blockchain) {
     rec = Vue.prototype.$blockchain;
   } else {
     rec = {
-      web3Portis: portisWeb3,
+      web3Portis: w3,
       web3Inject: undefined,
-      portis: portisInstance,
+      portis: instance,
       reader: undefined,
       submitter: undefined,
     };
@@ -30,44 +28,55 @@ function initOrUpdateVue(portisWeb3, portisLogged, portisInstance) {
   }
 
   Vue.prototype.$blockchain.getWeb3 = function () {
-    if (!portisLogged && rec.web3Inject) {
+    if (!isLogged && rec.web3Inject) {
       console.log('Web3 injected');
       return rec.web3Inject;
     }
     console.log('Web3 Portis');
     return rec.web3Portis;
   };
+}
 
-  function initBlockchainData() {
-    if (!Vue.prototype.$blockchainBasicData) {
-      Vue.prototype.$blockchainBasicData = blockchainBasicDataFactory(
-        Vue.prototype.$blockchain,
-        store,
-      );
-    }
-  }
-
-  const isEthereum = !(typeof ethereum === 'undefined');
-
-  if (isEthereum && portisLogged === false) {
-    ethereum.enable().then(() => {
-      rec.web3Inject = web3;
-      initBlockchainData();
-    });
-  } else {
-    initBlockchainData();
-  }
-
-
-  if (!initialized) {
-    new Vue({
-      router,
-      vuetify,
+function initBlockchainData() {
+  if (!Vue.prototype.$blockchainBasicData) {
+    Vue.prototype.$blockchainBasicData = blockchainBasicDataFactory(
+      Vue.prototype.$blockchain,
       store,
-      render: (h) => h(App),
-    }).$mount('#app');
-    initialized = true;
+    );
   }
+}
+
+function initOrUpdateVue(portisWeb3, portisLogged, portisInstance) {
+  Vue.config.productionTip = false;
+
+  return new Promise((res, rej) => {
+    setBlckchainInPrototype(portisWeb3, portisLogged, portisInstance);
+
+    const isEthereum = !(typeof ethereum === 'undefined');
+
+    if (isEthereum && portisLogged === false) {
+      ethereum.enable().then(() => {
+        Vue.prototype.$blockchain.web3Inject = web3;
+        initBlockchainData();
+        res(true);
+      }).catch((x) => {
+        rej(x);
+      });
+    } else {
+      initBlockchainData();
+      res(false);
+    }
+
+    if (!initialized) {
+      new Vue({
+        router,
+        vuetify,
+        store,
+        render: (h) => h(App),
+      }).$mount('#app');
+      initialized = true;
+    }
+  });
 }
 
 const portis = new Portis('ba1a2134-2bbe-441c-b856-5e8d13ebb80a', 'kovan', { gasRelay: true });
@@ -75,10 +84,13 @@ const web3Portis = new Web3(portis.provider);
 
 portis.isLoggedIn().then(({ result }) => {
   if (result === false) {
-    portis.showPortis().then(() => {
-      portis.changeNetwork('kovan');
+    initOrUpdateVue(web3Portis, false, portis).then((status) => {
+      if (!status) {
+        portis.showPortis().then(() => {
+          portis.changeNetwork('kovan');
+        });
+      }
     });
-    initOrUpdateVue(web3Portis, false, portis);
   } else {
     initOrUpdateVue(web3Portis, true, portis);
   }
